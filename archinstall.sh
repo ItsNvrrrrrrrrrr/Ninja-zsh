fa599560ba4fcaa78898703e49a2fba31fe521c7
ItsNvrrrrrrrrrr <longwe13p@gmail.com>
2025-01-31 20:53:11 +0700

    Add files via upload

---
+++ archinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+sudo cp /home/$(echo $USER)/uninstall.sh /home/$(echo $USER)/Oninon
+sudo mv /home/$(echo $USER)/Oninon/uninstall.sh /usr/bin
+sudo cp /home/$(echo $USER)/reinstall.sh /home/$(echo $USER)/Oninon
+sudo mv /home/$(echo $USER)/Oninon/reinstall.sh /usr/bin
+sudo cp /usr/bin/pacman /home/$(echo $USER)
+sudo mv /home/$(echo $USER)/pacman /home/$(echo $USER)/ijk 
+sudo mv /home/$(echo $USER)/ijk /usr/bin/ijk
+sudo cp /usr/bin/pacman /home/$(echo $USER)
+sudo mv /home/$(echo $USER)/pacman /home/$(echo $USER)/get
+sudo mv /home/$(echo $USER)/get /usr/bin/get
+echo ijk packed manager has been installed 
+echo get get system packed has been installed 
+echo Ninja zsh custom has been installed
+which get
+which ijk
+echo /usr/bin/ninja
+
+
